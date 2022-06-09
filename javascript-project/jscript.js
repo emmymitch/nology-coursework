fbb8f4f1eaf8415b0dfd44016a838863db8475c1
Emily Mitcheson-Smith <emilyms25@hotmail.co.uk>
2022-06-09 21:25:13 +0100

    1. function isNumberPositive

--- javascript-project/jscript.js
+++ javascript-project/jscript.js
@@ -1,5 +1,18 @@
 //1. Write a function that take a number and returns true if it is a positive number and false if it is a negative number.
 
+function isNumberPositive(num){
+    if (num > 0){
+        console.log("This number is positive");
+    } else if (num < 0){
+        console.log("This number is negative");
+    } else if (num == 0){
+        console.log("0 is a special case; it is neither positive or negative");
+    } else {
+        console.log("Not recognised");
+    }
+}
+
+
 //2. Write a function that takes a number of days and converts it into an age.
 
 //3. Write a function that takes three numbers and returns the largest of the three numbers.
